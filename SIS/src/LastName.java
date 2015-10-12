c24bf4e297d9264c436d10c2999dc40a469842de
benjacobs0518 <Ben.Jacobs@MU-C1-05.mullen.pvt.k12.co.us>
2015-10-12 14:57:30 -0600

    Last name

---
+++ SIS/src/LastName.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LastName
+	{
+
+		
+		public static void main(String[] args) throws IOException
+
+			{
+				sortName();
+			}
+		
+		public static void sortName() throws IOException
+			{
+				Scanner file = new Scanner(new File("studentlist3.txt"));
+				//lname
+				ArrayList<String> student = new ArrayList<String>();
+				
+				
+				while (file.hasNext())
+					{
+						student.add(file.nextLine());
+						
+					}
+			
+			for (int i = 0; i < student.size(); i++)
+				{
+					System.out.println(student.get(i));
+				}
+				
+			}
+	}
