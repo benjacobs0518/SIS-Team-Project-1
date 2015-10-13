7b8f32818d923c05f0d94c0ad288c4708b557d4f
tsskiracer <William.Starkey@MU-C1-03.mullen.pvt.k12.co.us>
2015-10-13 14:39:40 -0600

    Main menu finished

--- SIS/src/MainMenu.java
+++ SIS/src/MainMenu.java
@@ -8,6 +8,24 @@ public class MainMenu
 			System.out.println("    1) Add or delete a student");
 			System.out.println("    2) Change student grades/schedule");
 			System.out.println("    3) Sort students");
+			Scanner userInput = new Scanner(System.in);
+			String in=userInput.nextLine();
+			if (in.equals("1"))
+				{
+					//go to menu for adding and deleting a student
+				}
+			if (in.equals("2"))
+				{
+					//go to menu for student grades/schedule
+				}
+			if (in.equals("3"))
+				{
+					//go to menu for sorting students
+				}
+			else
+				{
+					System.out.println("Please restart and input a valid operator.");
+				}
 
 			}
 
