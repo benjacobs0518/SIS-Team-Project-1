84b085edf3b86c743a0b36fa522423b8b52000b3
Jonatan.Oliveira <Jonatan.Oliveira@MU-C1-04.mullen.pvt.k12.co.us>
2015-10-13 14:02:09 -0600

    Initial commit -Jona

---
+++ SIS/src/ChangeStudentGrade.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class ChangeStudentGrade 
+{
+
+	public static void main(String[] args) 
+		{
+		
+
+		}
+	public static void changeGrade()
+		{
+		System.out.println("Whose grade would you like to change?");
+		Scanner userInput = new Scanner(System.in);
+		int userChoice = userInput.nextInt();
+		
+		}
+	public static void switchClass()
+		{
+		System.out.println("Whose schedule would you like to change?");
+		Scanner userInput = new Scanner(System.in);
+		int userChoice = userInput.nextInt();
+		}
+
+}
