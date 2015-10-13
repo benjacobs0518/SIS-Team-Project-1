56430d8ab83682a0150b922d93905310dc7eff26
madeleine.simon <madeleine.simon@MU-C1-06.mullen.pvt.k12.co.us>
2015-10-13 14:20:27 -0600

    started add student

--- SIS/src/AddStudent.java
+++ SIS/src/AddStudent.java
@@ -1,5 +1,25 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddStudent
 	{
-		//madeleine was here
+	
+	public static void addStudent()
+		{
+		Scanner userInput = new Scanner(System.in);
+		LastName1.sortName();
+		System.out.println("Please input student first name.");
+		String fName = userInput.nextLine();
+		System.out.println("Please input student last name.");
+		
+		System.out.println("the students name is " + fname + " " + lName ".");
+		System.out.println("Please input (1) to select biology as the next period class, (2) to make english the next class, or (3) to make algebra the next class.");
+		int classInput = userInput.nextInt();
+		if (classInput == 1)
+			{
+				
+			}
+		}
 	}
