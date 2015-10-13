501ce8dc4e9d646eab2277ef68136bc5fbc8a7f8
benjacobs0518 <ben.jacobs@MU-C1-05.mullen.pvt.k12.co.us>
2015-10-13 15:01:50 -0600

    hh

--- SIS/src/LastName1.java
+++ SIS/src/LastName1.java
@@ -7,9 +7,25 @@ import java.util.Comparator;
 public class LastName1 implements Comparator<Student>
 	{
 		
-		public static void sortName()
-			{
+		
+
+		public int compare1(Student arg0, Student arg1)
+		{
+			
+		
+				 @Override
+				     public int compare(Student o1, Student o2) {
 				
+				         String name1 = o1.getlName();
+				
+				         String name2 = o2.getlName();
+				 
+				  
+				 
+				         // ascending order (descending order would be: name2.compareTo(name1))
+				 
+				         return name1.compareTo(name2);
+
 			}
 	}
 	
