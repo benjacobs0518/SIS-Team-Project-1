98c5f4aecaa6982f74cea0940ea2f0bb687293ab
Madeleine.Simon <Madeleine.Simon@MU-C1-06.mullen.pvt.k12.co.us>
2015-10-15 14:57:33 -0600

    end of day commit - madeleine

--- SIS/src/AddStudent.java
+++ SIS/src/AddStudent.java
@@ -17,17 +17,26 @@ public class AddStudent
 		classNames.add("English");
 		classNames.add("Algebra");
 		Scanner userInput = new Scanner(System.in);
+		Scanner userInput1 = new Scanner(System.in);
 		System.out.println("Please input student first name.");
 		String addedFirst = userInput.nextLine();
 		System.out.println("Please input student last name.");
 		String addedLast = userInput.nextLine();
 		System.out.println("Please input (1) to select biology as the next period class, (2) to make english the next class, or (3) to make algebra the next class.");
 		int period1Input = userInput.nextInt();
+		System.out.println("Input the grade for this class.");
+		String grade1Input = userInput1.nextLine();
 		System.out.println("Please select the next class.");
 		int period2Input = userInput.nextInt();
+		System.out.println("Input the grade for this class.");
+		String grade2Input = userInput1.nextLine();
+		System.out.println("Please select the next class.");
+		int period3Input = userInput.nextInt();
+		System.out.println("Please put in a grade for " + classNames.get(period3Input));
+		String grade3Input = userInput1.nextLine();
 		System.out.println("Input the students GPA.");
 		double inputGPA = userInput.nextDouble();
-		int period3Input = 
-		Student.students.add(new Student(addedFirst, addedLast, classNames.get(period1Input), classNames.get(period2Input), classNames.), inputGPA));
+		Runner.students.add(new Student(addedFirst, addedLast, classNames.get(period1Input), grade1Input, classNames.get(period2Input), grade2Input, classNames.get(period3Input), grade3Input, inputGPA));
+		System.out.println(Runner.students);
 		}
 	}
